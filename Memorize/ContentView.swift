bad2cf070c9b9c7fd14c7b0933f52f0dfd2c125b
meamruri <meammf@yandex.ru>
2020-09-01 18:47:51 +0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Rom Dr on 01.09.2020.
+//  Copyright © 2020 Roman Dronov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
