6929cf18a3b38791d4ad7222e1b02ff261965d5f
meamruri <meammf@yandex.ru>
2020-09-23 18:00:07 +0300

    Lecture 7: Multithreading

--- EmojiArt/EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -9,4 +9,55 @@ import SwiftUI
 
 class EmojiArtDocument : ObservableObject {
     static let pallete: String = "🎾🍉🐭🤠🚤📸✅🇮🇪"
+    
+    @Published private var emojiArt: EmojiArt = EmojiArt()
+    
+    @Published private(set) var backgroundImage: UIImage?
+    
+    var emojis: [EmojiArt.Emoji] { emojiArt.emojis }
+    
+    // MARK: - intent(s)
+    
+    func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
+        emojiArt.addEmoji(emoji, x: Int(location.x), y: Int(location.y), size: Int(size))
+    }
+    
+    func moveEmoji(_ emoji: EmojiArt.Emoji, by offset: CGSize) {
+        if let emojiIndex = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[emojiIndex].x += Int(offset.width)
+            emojiArt.emojis[emojiIndex].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        if let emojiIndex = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[emojiIndex].size = Int((CGFloat(emojiArt.emojis[emojiIndex].size) * scale).rounded(.toNearestOrEven))
+        }
+    }
+    
+    func setBackgroundUrl(_ url: URL?) {
+        emojiArt.backgroundURL = url?.imageURL
+        fetchBackgroundImageData()
+    }
+    
+    private func fetchBackgroundImageData() {
+        backgroundImage = nil
+        if let url = self.emojiArt.backgroundURL {
+            DispatchQueue.global(qos: .userInitiated).async {
+                if let imageData = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        if url == self.emojiArt.backgroundURL {
+                            self.backgroundImage = UIImage(data: imageData)
+                        }                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+extension EmojiArt.Emoji {
+    var fontSize: CGFloat { CGFloat(self.size) }
+    var location: CGPoint { CGPoint(x: self.x, y: self.y)}
 }
--- EmojiArt/EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -11,10 +11,60 @@ struct EmojiArtDocumentView: View {
     @ObservedObject var document: EmojiArtDocument
     
     var body: some View {
-        HStack {
-            ForEach(EmojiArtDocument.pallete.map { String($0) }, id: \.self) { emoji in
-                Text(emoji)
+        VStack {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(EmojiArtDocument.pallete.map { String($0) }, id: \.self) { emoji in
+                        Text(emoji)
+                            .font(Font.system(size: defaultEmojiSize))
+                            .onDrag { return NSItemProvider(object: emoji as NSString) }
+                    }
+                }
+            }
+            .padding(.horizontal)
+            GeometryReader { geometry in
+                Color.white.overlay(
+                    Group {
+                        if document.backgroundImage != nil {
+                            Image(uiImage: document.backgroundImage!)
+                        }
+                    }
+                )
+                    .edgesIgnoringSafeArea([.horizontal, .bottom])
+                    .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { providers, location in
+                        var location = geometry.convert(location, from: .global)
+                        location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
+                        return drop(providers: providers, at: location)
+                    }
+                ForEach(self.document.emojis) { emoji in
+                    Text(emoji.text)
+                        .font(self.font(for: emoji))
+                        .position(self.position(for: emoji, in: geometry.size))
+                }
             }
         }
     }
+    
+    private func font(for emoji: EmojiArt.Emoji) -> Font {
+        Font.system(size: emoji.fontSize )
+    }
+    
+    private func position(for emoji: EmojiArt.Emoji, in size: CGSize) -> CGPoint {
+        CGPoint(x: emoji.location.x + size.width/2, y: emoji.location.y + size.height/2)
+    }
+    
+    private func drop(providers: [NSItemProvider], at location: CGPoint) -> Bool {
+        var found = providers.loadFirstObject(ofType: URL.self) { url in
+            self.document.setBackgroundUrl(url)
+        }
+        
+        if !found {
+            found = providers.loadObjects(ofType: String.self) { string in
+                self.document.addEmoji(string, at: location, size: defaultEmojiSize)
+            }
+        }
+        return found
+    }
+    
+    private let defaultEmojiSize: CGFloat = 48
 }
